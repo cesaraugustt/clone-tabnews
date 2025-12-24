5f02dd7961c7659ac62171735b3e6740a872b353
César Augusto <103086699+cesaraugustt@users.noreply.github.com>
2025-12-24 00:02:48 +0000

    finished `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,22 +6,26 @@ async function status(request, response) {
   const dbVersionResult = await database.query("SHOW server_version;");
   const dbVersionValue = dbVersionResult.rows[0].server_version;
 
-  const psqlMaxConnections = (await database.query("SHOW max_connections;"))
+  const dbMaxConnections = (await database.query("SHOW max_connections;"))
     .rows[0].max_connections;
 
-  const psqlActiveConnections = (
-    await database.query("SELECT * FROM pg_stat_activity WHERE state='active';")
-  ).rowCount;
+  const dbName = process.env.POSTGRES_DB;
+  const dbActiveConnections = (
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+      values: [dbName],
+    })
+  ).rows[0].count;
 
   response.status(200).json({
     update_at: updateAt,
     dependencies: {
       database: {
         version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnections),
+        active_connections: dbActiveConnections,
       },
     },
-    postgres_max_connections: psqlMaxConnections,
-    postgres_active_connections: psqlActiveConnections,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,14 +3,10 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
-
-  expect(responseBody.dependencies.database.version).toBe("16.0");
-
-  expect(responseBody.postgres_max_connections).toBeDefined();
-
-  expect(responseBody.postgres_active_connections).toBeDefined();
-
   const parseUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.active_connections).toBe(1);
 });
